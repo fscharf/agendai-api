b279ceaa88e1065b1bdffa906f6bbde58ade937e
Felipe Scharf <asphx@outlook.com>
2021-05-22 11:36:58 -0300

    Heroku Settings

--- src/services/db.js
+++ src/services/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
-const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-const sequelize = new Sequelize("barber_shop_api", DB_USERNAME, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
   define: {
