0027db8cf38ab31c5a082ea8ebc6a4fcfb7bb0ad
Felipe Scharf <asphx@outlook.com>
2021-05-22 11:52:45 -0300

    Heroku Settings

--- src/services/db.js
+++ src/services/db.js
@@ -5,17 +5,16 @@ const pg = require("pg");
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: true,
+  },
   define: {
     timestamps: false,
     freezeTableName: true,
     camelCase: false,
   },
 });
-
-var conString =
-  "postgres://jjghjgeisymkam:5a07a683f279d29992de78b75f9e7a04e2b734cc04a831eeb3b0b454b948365b@ec2-52-4-171-132.compute-1.amazonaws.com:5432/des24b39usu0cf";
-var client = new pg.Client(conString);
-client.connect();
 
 try {
   sequelize.authenticate();
