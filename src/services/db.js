d903a61738a49ed02db582a415184c5b9eb537ef
Felipe Scharf <asphx@outlook.com>
2021-05-22 12:07:04 -0300

    Heroku Settings

--- src/services/db.js
+++ src/services/db.js
@@ -5,6 +5,9 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
   ssl: true,
+  dialectOptions: {
+    ssl: true,
+  },
   define: {
     timestamps: false,
     freezeTableName: true,
