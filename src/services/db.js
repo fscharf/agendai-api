95ffb587aefa661e52ef2f9e5c04da6d49a37cc0
Felipe Scharf <asphx@outlook.com>
2021-05-22 11:49:45 -0300

    Heroku Settings

--- src/services/db.js
+++ src/services/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+const pg = require("pg");
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
@@ -10,6 +11,11 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
     camelCase: false,
   },
 });
+
+var conString =
+  "postgres://jjghjgeisymkam:5a07a683f279d29992de78b75f9e7a04e2b734cc04a831eeb3b0b454b948365b@ec2-52-4-171-132.compute-1.amazonaws.com:5432/des24b39usu0cf";
+var client = new pg.Client(conString);
+client.connect();
 
 try {
   sequelize.authenticate();
