46b8e5b8ba3bb5f8d375297919f5037359c0e1c0
Felipe Scharf <asphx@outlook.com>
2021-05-22 12:09:37 -0300

    Heroku Settings

--- src/services/db.js
+++ src/services/db.js
@@ -4,9 +4,11 @@ const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
-  ssl: true,
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
   },
   define: {
     timestamps: false,
