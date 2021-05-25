eb3c95c40f5886673f37ec2ccd22794290ee028b
Felipe Scharf <asphx@outlook.com>
2021-05-25 10:51:58 -0300

    Cors Settings

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require("./routes");
 const port = process.env.PORT;
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000.com");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
