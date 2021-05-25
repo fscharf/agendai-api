db9b3eca1ab7205d1ce606b73f59308d14d01c8d
Felipe Scharf <asphx@outlook.com>
2021-05-25 11:33:00 -0300

    Cors Settings

--- routes.js
+++ routes.js
@@ -9,20 +9,20 @@ router.get("/", (req, res) => {
   res.json({ info: "Barber Shop API" });
 });
 
-router.post("/users/signin", auth.signIn);
-router.get("/verifyToken", auth.verifyToken);
-router.get("/confirm/:confirmationCode", auth.verifyUser);
+router.post("/users/signin", ValidationMiddleware.validJWTNeeded, auth.signIn);
+router.get("/verifyToken", ValidationMiddleware.validJWTNeeded, auth.verifyToken);
+router.get("/confirm/:confirmationCode", ValidationMiddleware.validJWTNeeded, auth.verifyUser);
 
 router.get("/users", ValidationMiddleware.validJWTNeeded, user.getUsers);
-router.get("/users/:id", user.getUserById);
-router.post("/users", user.createUser);
-router.put("/users/:id", user.updateUser);
-router.delete("/users/:id", user.deleteUser);
+router.get("/users/:id", ValidationMiddleware.validJWTNeeded, user.getUserById);
+router.post("/users", ValidationMiddleware.validJWTNeeded, user.createUser);
+router.put("/users/:id", ValidationMiddleware.validJWTNeeded, user.updateUser);
+router.delete("/users/:id", ValidationMiddleware.validJWTNeeded, user.deleteUser);
 
-router.get("/schedule", schedule.getSchedule);
-router.get("/schedule/:id", schedule.getScheduleById);
-router.post("/schedule", schedule.createSchedule);
-router.put("/schedule/:id", schedule.updateSchedule);
-router.delete("/schedule/:id", schedule.deleteSchedule);
+router.get("/schedule", ValidationMiddleware.validJWTNeeded, schedule.getSchedule);
+router.get("/schedule/:id", ValidationMiddleware.validJWTNeeded, schedule.getScheduleById);
+router.post("/schedule", ValidationMiddleware.validJWTNeeded, schedule.createSchedule);
+router.put("/schedule/:id", ValidationMiddleware.validJWTNeeded, schedule.updateSchedule);
+router.delete("/schedule/:id", ValidationMiddleware.validJWTNeeded, schedule.deleteSchedule);
 
 module.exports = router;
