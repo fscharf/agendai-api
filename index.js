31be337c29ee7206791cc84c17d38428f7bf893a
Felipe Scharf <asphx@outlook.com>
2021-06-02 16:14:02 -0300

    Reset Password Settings

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, GET, POST, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
