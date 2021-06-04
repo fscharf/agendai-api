ed1f827a1a07278cbcba326ab45a01963b61ade4
Felipe Scharf <asphx@outlook.com>
2021-06-04 15:52:00 -0300

    Schedule Hour Settings

--- src/models/schedule.hour.model.js
+++ src/models/schedule.hour.model.js
@@ -1,4 +1,6 @@
 const db = require("../services/db");
+const Schedule = require("./schedule.model");
+const User = require("./user.model");
 
 const ScheduleHour = db.sequelize.define("schedule_hour", {
   _id: {
@@ -10,6 +12,20 @@ const ScheduleHour = db.sequelize.define("schedule_hour", {
   hour: {
     type: db.Sequelize.TIME,
   },
+  date: {
+    type: db.Sequelize.DATEONLY,
+  },
+  description: {
+    type: db.Sequelize.STRING,
+  }
+});
+
+ScheduleHour.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+ScheduleHour.belongsTo(Schedule, {
+  foreignKey: "schedule_id",
 });
 
 // !Important: 'sync' and 'force:true' will drop the table with the specified columns in model
