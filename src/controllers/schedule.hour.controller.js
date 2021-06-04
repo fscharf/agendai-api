0bc595464803d5b081f355f986fd28aee2b25e9c
Felipe Scharf <asphx@outlook.com>
2021-06-04 14:51:11 -0300

    Schedule Hour Settings

--- src/controllers/schedule.hour.controller.js
+++ src/controllers/schedule.hour.controller.js
@@ -1,30 +1,34 @@
 const ScheduleHour = require("../models/schedule.hour.model");
 
-const getScheduleHour = (req, res) => {
-  ScheduleHour.findAll()
+const getScheduleHour = async (req, res) => {
+  await ScheduleHour.findAll()
     .then((result) => res.status(200).send(result))
     .catch((err) => res.status(400).send({ error: true, message: err }));
 };
 
-const createScheduleHour = (req, res) => {
+const createScheduleHour = async (req, res) => {
   const hour = req.body.hour;
 
-  ScheduleHour.create({
+  await ScheduleHour.create({
     hour: hour,
   })
-    .then(() => res.status(200))
+    .then(() =>
+      res.status(200).send({ error: false, message: "Adicionado com sucesso!" })
+    )
     .catch((err) => res.status(400).send({ error: true, message: err }));
 };
 
-const deleteScheduleHour = (req, res) => {
+const deleteScheduleHour = async (req, res) => {
   const id = req.params.id;
 
-  ScheduleHour.destroy({
+  await ScheduleHour.destroy({
     where: {
       _id: id,
     },
   })
-    .then(() => res.status(200))
+    .then(() =>
+      res.status(200).send({ error: false, message: "Excluído com sucesso!" })
+    )
     .catch((err) => res.status(400).send({ error: true, message: err }));
 };
 
