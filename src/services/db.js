c1fc564dc8fa8697c418a80a8b295895663e87fa
Felipe Scharf <asphx@outlook.com>
2021-06-04 12:39:14 -0300

    Schedule Hour Settings

--- src/services/db.js
+++ src/services/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
     }
   },
   define: {
-    timestamps: true,
+    timestamps: false,
     freezeTableName: true,
     camelCase: false,
   },
