8eb4f7c956e6d0fd368f53fa04ed3423eea7689f
Felipe Scharf <asphx@outlook.com>
2021-06-12 13:58:18 -0300

    Route Settings

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const app = express();
 const router = require("./src/routes");
 const port = process.env.PORT;
 const cors = require("cors");
-app.use(
-  cors({
-    credentials: true,
-    allowedHeaders: "*",
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "*"); // allowed actios
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // to deal with chrome sending an extra options request
+  }
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 app.use(express.json());
 app.use(
