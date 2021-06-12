ff059d10fd9204b429ff2c925c7a0e4504417fb6
Felipe Scharf <asphx@outlook.com>
2021-06-12 11:17:09 -0300

    Cors Settings

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ const app = express();
 const router = require("./src/routes");
 const port = process.env.PORT;
 const cors = require("cors");
+app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  ); // allowed actios
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // to deal with chrome sending an extra options request
+  }
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
-
-app.use(cors());
 
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: false,
+    extended: true,
   })
 );
 
