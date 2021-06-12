7680a935b9ed4a808742d66411f6b39d6be2c8dd
Felipe Scharf <asphx@outlook.com>
2021-06-12 14:15:18 -0300

    Schedule Settings

--- src/models/schedule.model.js
+++ src/models/schedule.model.js
@@ -37,6 +37,6 @@ Schedule.belongsTo(ScheduleHour, {
 });
 
 // !Important: 'sync' and 'force:true' will drop the table with the specified columns in model
-// Schedule.sync({force: true})
+Schedule.sync({force: true})
 
 module.exports = Schedule;
