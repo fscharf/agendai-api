a8c538be01c8c7fb9165e604eae0ca96d85b5a26
Felipe Scharf <asphx@outlook.com>
2021-06-12 10:50:31 -0300

    Cors Settings

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const router = require("./src/routes");
 const port = process.env.PORT;
 const cors = require("cors");
 
-app.use(cors());
-
 app.use((req, res, next) => {
   // Website you wish to allow to connect
   res.setHeader(
@@ -18,6 +16,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
+
+app.use(cors());
+
 
 app.use(bodyParser.json());
 app.use(
