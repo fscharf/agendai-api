f8a7ae94dc0c019cfe9506da0acc85c75da2313b
Felipe Scharf <asphx@outlook.com>
2021-06-22 14:44:08 -0300

    CtrlSystem API Settings

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const router = require("./src/routes");
 const port = process.env.PORT;
 const cors = require("cors");
 
+app.use(cors());
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "*"); // allowed actios
   if (req.method === "OPTIONS") {
     return res.sendStatus(200); // to deal with chrome sending an extra options request
@@ -16,8 +18,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
-
-app.use(cors());
 
 app.use(express.json());
 app.use(
